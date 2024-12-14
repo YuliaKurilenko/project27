09d525831d1c300ff98f392e2213ed8884b9a1f3
YuliaKurilenko <162790631+YuliaKurilenko@users.noreply.github.com>
2024-12-14 12:59:13 +0300

    Add files via upload

---
+++ board/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+app_name = 'board'
+
+urlpatterns = [
+    path('', views.advertisement_list, name='advertisement_list'),
+    path('advertisement/<int:pk>/', views.advertisement_detail, name='advertisement_detail'),
+    path('add/', views.add_advertisement, name='add_advertisement'),
+    path('edit/<int:pk>/', views.edit_advertisement, name='edit_advertisement'),
+    path('delete/<int:pk>/', views.delete_advertisement, name='delete_advertisement'),
+
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
